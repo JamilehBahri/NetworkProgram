4f59b6b5d7f05379902e101a28b002cb8d5e0d51
jamilehbahri <jamilebahri@yahoo.com>
2017-08-13 18:02:06 +0430

    Chatroom with single thread (commit 3)

--- src/main/java/ir/phgint/Client.java
+++ src/main/java/ir/phgint/Client.java
@@ -10,52 +10,45 @@ public class Client {
         BufferedReader socketReader = null;
         BufferedWriter socketWriter = null;
         try {
-// Create a socket that will connect to localhost
-// at port 12900. Note that the server must also be
-// running at localhost and 12900.
+            // Create a socket that will connect to localhost
             socket = new Socket("localhost", 1100);
-            System.out.println("Started client socket at " +
-                    socket.getLocalSocketAddress());
-// Create a buffered reader and writer using the socket's
-// input and output streams
-            socketReader = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            socketWriter = new BufferedWriter(
-                    new OutputStreamWriter(socket.getOutputStream()));
-// Create a buffered reader for user's input
-            BufferedReader consoleReader =
-                    new BufferedReader(new InputStreamReader(System.in));
-            String promptMsg = "Please enter a message (Bye to quit):";
+            System.out.println("Started client socket at " + socket.getLocalSocketAddress());
+            // Create a buffered reader and writer using the socket's input and output streams
+            socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            socketWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            // Create a buffered reader for user's input
+            BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
             String outMsg = null;
-            System.out.print(promptMsg);
+            System.out.print("Please enter a message (Bye to quit):");
             while ((outMsg = consoleReader.readLine()) != null) {
                 if (outMsg.equalsIgnoreCase("bye")) {
                     break;
                 }
-                // Add a new line to the message to the server,
-// because the server reads one line at a time.
+                // Add a new line to the message to the server
                 socketWriter.write(outMsg);
                 socketWriter.write("\n");
                 socketWriter.flush();
-// Read and display the message from the server
-//                String inMsg = socketReader.readLine();
-//                System.out.println("Server: " + inMsg);
-//                System.out.println(); // Print a blank line
-                System.out.print(promptMsg);
+                System.out.print("Please enter a message (Bye to quit):");
+
             }
         } catch (IOException e) {
             e.printStackTrace();
-//        } finally {
-//// Finally close the socket
-//            if (socket != null) {
-//                try {
-//                    socket.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
+    public static void readPm() {
+//      int inMsg = socketReader.read();
+        System.out.println("Server: ");
+
+    }
 
 }
--- src/main/java/ir/phgint/SingleThread.java
+++ src/main/java/ir/phgint/SingleThread.java
@@ -13,34 +13,35 @@ public class SingleThread extends Thread {
     private boolean isRunning = true;
 
     void addSocket(Socket socket) {
-       socketList.add(socket);
+        socketList.add(socket);
     }
 
-    void removeSocket(Socket socket){
+    void removeSocket(Socket socket) {
         socketList.remove(socket);
     }
-
-//    public ArrayList<Socket> getSocketList() {
-//        return socketList;
-//    }
 
     public void setSocketList(List<Socket> socketList) {
         this.socketList = socketList;
     }
 
-    public void run(){
-        while(isRunning){
-            for(Socket socket:socketList) {
-
-                handleClientRequest(socket);
-
+    public void run() {
+        while (isRunning) {
+            for (Socket socket : socketList) {
+                try {
+                    if (socket.getInputStream().available() != 0) {
+                        handleClientRequest(socket);
+                    }
+                    // next socket
+                    continue;
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
         }
     }
 
@@ -48,42 +49,28 @@ public class SingleThread extends Thread {
         BufferedReader socketReader = null;
         BufferedWriter socketWriter = null;
         try {
-// Create a buffered reader and writer for teh socket
+            // Create a buffered reader and writer for teh socket
             socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             socketWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-            String inMsg = null;
-            while ((inMsg = socketReader.readLine()) != null) {
-                System.out.println("Received from client: " + inMsg);
-// Echo the received message to the client
-                for(Socket socketGuest:socketList){
-                    if(socketGuest != socket){
-                        String outMsg = inMsg;
-                        socketWriter.write(outMsg);
-                        socketWriter.write("\n");
-                        socketWriter.flush();
-                    }
-                    else{
-                        continue;
-                    }
-                }
-//                try {
-//                    Thread.sleep(1000);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
+            int bytesRead;
+            byte[] buffer = new byte[1024];
+            InputStream is = socket.getInputStream();
+            bytesRead = is.read(buffer, 0, buffer.length);
+            System.out.println("Received from client: " + bytesRead);
+            // Echo the received message to the client   //broad cast
+            for (Socket socketGuest : socketList) {
+                if (socketGuest != socket) {
+                    socketWriter.write(bytesRead);
+                    socketWriter.write("\n");
+                    socketWriter.flush();
+                    Client.readPm();
 
+                } else {
+                    continue;
+                }
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
-            try {
-                socket.close();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
